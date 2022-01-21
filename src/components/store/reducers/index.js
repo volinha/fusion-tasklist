7694f73562accba96c834e81100151fb8a90293d
Vinícius Fernandes <viniciusfernandesmct@gmail.com>
2022-01-21 19:33:09 -0300

    updated: added dialog reducer to combineReducers

--- src/components/store/reducers/index.js
+++ src/components/store/reducers/index.js
@@ -3,9 +3,11 @@ import { combineReducers } from 'redux';
 import tasksReducer from './tasks';
 import searchReducer from './search';
 import tagsReducer from './tags';
+import dialogReducer from './dialog';
 
 export default combineReducers({
     tasks: tasksReducer,
     search: searchReducer,
-    tags: tagsReducer
+    tags: tagsReducer,
+    dialog: dialogReducer
 })

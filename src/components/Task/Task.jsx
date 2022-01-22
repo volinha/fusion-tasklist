41d456b39b4e43f2b20ec7f778c3ef68d15a08ed
Vinícius Fernandes <viniciusfernandesmct@gmail.com>
2022-01-22 09:05:03 -0300

    updated: task card date and priority

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -1,25 +1,25 @@
-import { useDispatch } from 'react-redux';
-import TaskActions from '../store/actions/tasks';
-import DialogActionsRedux from '../store/actions/dialog';
+import { useDispatch } from "react-redux";
+import TaskActions from "../store/actions/tasks";
+import DialogActionsRedux from "../store/actions/dialog";
 
-import { Chip, Grid, Tooltip } from '@mui/material';
-import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
+import { Chip, Grid, Tooltip } from "@mui/material";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
 
-import styled from 'styled-components';
+import styled from "styled-components";
 
-import WarningIcon from '@mui/icons-material/Warning';
-import EditIcon from '@mui/icons-material/Edit';
-import CheckIcon from '@mui/icons-material/Check';
-import DeleteIcon from '@mui/icons-material/Delete';
-import CommentIcon from '@mui/icons-material/Comment';
+import ArrowRightIcon from '@mui/icons-material/ArrowRight';
+import EditIcon from "@mui/icons-material/Edit";
+import CheckIcon from "@mui/icons-material/Check";
+import DeleteIcon from "@mui/icons-material/Delete";
+import CommentIcon from "@mui/icons-material/Comment";
 
-import OpenDialog from '../Dialog/Dialog';
-import { useSelector } from 'react-redux';
+import OpenDialog from "../Dialog/Dialog";
+import { useSelector } from "react-redux";
 
 /* const Tags = styled.button`
     font-family: "Roboto", Helvetica;
@@ -44,99 +44,167 @@ import { useSelector } from 'react-redux';
 ` */
 
 const CardButtons = styled(Button)`
-    width: 100%;
-    margin: 4px -4px !important;
+  width: 100%;
+  margin: 4px -4px !important;
 
-    font-family: "Roboto", Helvetica !important;
-    letter-spacing: 1px !important;
-`
+  font-family: "Roboto", Helvetica !important;
+  letter-spacing: 1px !important;
+`;
 
 export default function Task({ task, done }) {
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch();
+  const colorSelector = (priority) => {
+      if(priority === 'urgente') return '#f44336'
+      if(priority === 'importante') return '#ff9800'
+      if(priority === 'normal') return '#4caf50'
+  } 
 
-    const allTasks = useSelector(state => state.tasks.items);
-
-    return (
-        <Box style={{ marginTop: '8px' }}>
-            <OpenDialog
-                type="delete-task"
-            />
-            <Card variant="outlined" >
-                <Grid container columns={12}>
-                    <Grid item xs={8}>
-                        <CardContent>
-                            <Typography variant="h5" component="div">
-                                {task.title}
-                            </Typography>
-                            <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                                {task.priority}
-                            </Typography>
-                            {task.tags.map((item, index) => {
-                                return (
-                                    <Chip
-                                        key={index}
-                                        label={item.value}
-                                        onClick={() => console.log("click")}
-                                        icon={<WarningIcon fontSize="small" />}
-                                        style={{marginTop: '2px'}}
-                                    />
-                                )
-                            })
-                            }
-                        </CardContent>
-                    </Grid>
-                    <Grid item xs={4}>
-                        <Grid container direction="column" alignItems="flex-end" spacing={6} >
-                            <Grid item>
-                                <Button>
-                                    {done ? 
-                                    <CommentIcon onClick={() => dispatch(DialogActionsRedux.OpenDialog(task.id, task.title, 'viewComment'))} />
-                                    : 
-                                    <EditIcon />}
-                                </Button>
-                            </Grid>
-                            <Grid item>
-                                {done ?
-                                    <CardButtons
-                                        color="info"
-                                        sx={{ cursor: 'default' }}
-                                    >
-                                        <CheckIcon />Finalizada
-                                    </CardButtons>
-                                    :
-                                    <>
-                                        <CardButtons
-                                            variant="contained"
-                                            color="success"
-                                            onClick={() => dispatch(DialogActionsRedux.OpenDialog(task.id, task.title, 'finishtask'))}
-                                        >
-                                            <CheckIcon />Concluir
-                                        </CardButtons>
-                                        <CardButtons
-                                            variant="contained"
-                                            color="error"
-                                            onClick={() => dispatch(DialogActionsRedux.OpenDialog(task.id, task.title, 'task'))}
-                                        >
-                                            <DeleteIcon />Cancelar
-                                        </CardButtons>
-                                    </>
-                                }
-
-                            </Grid>
-                        </Grid>
-                    </Grid>
-                </Grid>
-                <CardActions>
-                    <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                        {done ?
-                            "Finalizado em: " + new Date(task.finishedAt).toLocaleString()
-                            :
-                            "Criado em: " + new Date(task.createdAt).toLocaleString()
-                        }
-                    </Typography>
-                </CardActions>
-            </Card>
-        </Box>
-    );
+  return (
+    <Box style={{ marginTop: "8px" }}>
+      <OpenDialog type="delete-task" />
+      <Card variant="outlined">
+        <Grid container columns={12}>
+          <Grid item xs={8}>
+            <CardContent>
+              <Typography variant="h5" component="div">
+                {task.title}
+              </Typography>
+              <Typography sx={{ mb: 1.5 }} color="text.secondary">
+              <Button
+                variant="outlined"
+                disabled={task.status === 'done'}
+                sx={{
+                  color: `${colorSelector(task.priority) + "99"}`,
+                  borderColor: `${colorSelector(task.priority) + "99"}`,
+                  height: "1.5em",
+                  minWidth: "1.5em",
+                  textTransform: "lowercase",
+                  '&:hover': {
+                    color: `${colorSelector(task.priority)}`,
+                    borderColor: `${colorSelector(task.priority)}`,
+                  }
+                }}
+              >
+                {task.priority}
+              </Button>
+                {done
+                  ? ""
+                  : " - até " +
+                    new Date(task.date).toLocaleString().slice(0, 10)}
+              </Typography>
+              {task.tags.map((item, index) => {
+                return (
+                  <Chip
+                    key={index}
+                    label={item.value}
+                    onClick={() => console.log("click")}
+                    icon={<ArrowRightIcon fontSize="small" />}
+                    style={{ marginTop: "2px" }}
+                    disabled={task.status === 'done'}
+                  />
+                );
+              })}
+            </CardContent>
+          </Grid>
+          <Grid item xs={4}>
+            <Grid
+              container
+              direction="column"
+              alignItems="flex-end"
+              spacing={6}
+            >
+              <Grid item>
+                <Button>
+                  {done ? (
+                    <CommentIcon
+                      onClick={() =>
+                        dispatch(
+                          DialogActionsRedux.OpenDialog(
+                            task.id,
+                            task.title,
+                            "viewComment"
+                          )
+                        )
+                      }
+                    />
+                  ) : (
+                    <EditIcon onClick={() => console.log(task.tags)} />
+                  )}
+                </Button>
+              </Grid>
+              <Grid item>
+                {done ? (
+                  <>
+                    <CardButtons color="info" sx={{ cursor: "default" }}>
+                      <CheckIcon />
+                      Finalizada
+                    </CardButtons>
+                    <CardButtons
+                      variant="contained"
+                      color="error"
+                      onClick={() =>
+                        dispatch(
+                          DialogActionsRedux.OpenDialog(
+                            task.id,
+                            task.title,
+                            "task"
+                          )
+                        )
+                      }
+                    >
+                      <DeleteIcon />
+                      Remover
+                    </CardButtons>
+                  </>
+                ) : (
+                  <>
+                    <CardButtons
+                      variant="contained"
+                      color="success"
+                      onClick={() =>
+                        dispatch(
+                          DialogActionsRedux.OpenDialog(
+                            task.id,
+                            task.title,
+                            "finishtask"
+                          )
+                        )
+                      }
+                    >
+                      <CheckIcon />
+                      Concluir
+                    </CardButtons>
+                    <CardButtons
+                      variant="contained"
+                      color="error"
+                      onClick={() =>
+                        dispatch(
+                          DialogActionsRedux.OpenDialog(
+                            task.id,
+                            task.title,
+                            "task"
+                          )
+                        )
+                      }
+                    >
+                      <DeleteIcon />
+                      Remover
+                    </CardButtons>
+                  </>
+                )}
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+        <CardActions>
+          <Typography sx={{ mb: 1.5 }} color="text.secondary">
+            {done
+              ? "Finalizado em: " + new Date(task.finishedAt).toLocaleString()
+              : "Criado em: " + new Date(task.createdAt).toLocaleString()}
+          </Typography>
+        </CardActions>
+      </Card>
+    </Box>
+  );
 }

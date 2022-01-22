3febb3d303379e84424b90836dbfa52b6673e02c
Vinícius Fernandes <viniciusfernandesmct@gmail.com>
2022-01-22 09:04:16 -0300

    updated: tag filtering

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,74 +1,64 @@
-import { Grid } from '@mui/material';
-import React from 'react';
-import { useSelector } from 'react-redux';
-import Container from '../../components/Container/Container';
-import Task from '../../components/Task/Task';
+import { Grid } from "@mui/material";
+import React from "react";
+import { useSelector } from "react-redux";
+import Container from "../../components/Container/Container";
+import Task from "../../components/Task/Task";
 
 const Home = () => {
-    const tasks = useSelector(state => state.tasks.items);
-    const searchValue = useSelector(state => state.search.value);
+  const tasks = useSelector((state) => state.tasks.items);
+  const searchValue = useSelector((state) => state.search.value);
 
-    const filteredTasks = !!searchValue ? 
-    tasks.filter(task => {
-        return  task.title.toLowerCase().includes(searchValue.toLowerCase()) ||
-                task.status.toLowerCase().includes(searchValue.toLowerCase()) ||
-                task.priority.toLowerCase().includes(searchValue.toLowerCase());
-    })
+  const filteredTasks = !!searchValue
+    ? tasks.filter((task) => {
+        return (
+          task.title.toLowerCase().includes(searchValue.toLowerCase()) ||
+          task.status.toLowerCase().includes(searchValue.toLowerCase()) ||
+          task.priority.toLowerCase().includes(searchValue.toLowerCase()) ||
+          task.tags.some((item) => item.value.toLowerCase().includes(searchValue.toLowerCase()))
+        );
+      })
     : tasks;
 
-    var openTasks = filteredTasks.filter(item => {
-        return item.status === 'open';
-    });
+  var openTasks = filteredTasks.filter((item) => {
+    return item.status === "open";
+  });
 
-    var doneTasks = filteredTasks.filter(item => {
-        return item.status === 'done';
-    });
+  var doneTasks = filteredTasks.filter((item) => {
+    return item.status === "done";
+  });
 
-    return (
-        <div>
-            <Grid 
-            container 
-            spacing={12} 
-            columns={12}
-            justifyContent="center"
-            direction="row"
-            >
-            <Grid item xs={5}>
-                <Container 
-                    title="Abertas"
-                    quantity={openTasks.length}
-                >
-                    {openTasks.map(item => {
-                        return(
-                            <Task
-                                key={item.id}
-                                task={item}
-                            />
-                        )
-                    })}
-                </Container>
-            </Grid>
-            <Grid item xs={5}>
-                <Container 
-                    title="Concluídas"
-                    quantity={doneTasks.length}
-                >
-                    {doneTasks.map(item => {
-                        return(
-                            <Task
-                                key={item.id}
-                                task={item}
-                                done
-                            > 
-                            {item.title}
-                            </Task>
-                        )
-                    })}
-                </Container>
-            </Grid>
-            </Grid>
-        </div>
-    );
+  return (
+    <div>
+      <Grid
+        container
+        spacing={12}
+        columns={12}
+        justifyContent="center"
+        direction="row"
+      >
+        <Grid item xs={5}>
+          <Container title="Abertas" quantity={openTasks.length}>
+            {openTasks
+              .slice(0)
+              .reverse()
+              .map((item) => {
+                return <Task key={item.id} task={item} />;
+              })}
+          </Container>
+        </Grid>
+        <Grid item xs={5}>
+          <Container title="Concluídas" quantity={doneTasks.length}>
+            {doneTasks
+              .slice(0)
+              .reverse()
+              .map((item) => {
+                return <Task key={item.id} task={item} done />;
+              })}
+          </Container>
+        </Grid>
+      </Grid>
+    </div>
+  );
 };
 
 export default Home;
